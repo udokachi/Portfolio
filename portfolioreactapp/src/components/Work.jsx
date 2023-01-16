275ef66c0ece61b202080eef093e6361f546193e
udokachi <chidinma242@gmail.com>
2023-01-16 22:50:01 +0100

    contactAndLinks

--- portfolioreactapp/src/components/Work.jsx
+++ portfolioreactapp/src/components/Work.jsx
@@ -7,7 +7,7 @@ const Work = () => {
     // projects file
     const project = data;
     //setProject(data);
-  
+
   return (
     <div name='work' className='w-full md:h-screen text-gray-300 bg-[#1d2a3c]'>
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
